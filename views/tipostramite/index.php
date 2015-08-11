8c3a05785a475340fa604c1a01971e269108f418
Juan Carlos Robles Medina <JuanCarlos@MacBook-Pro-de-Juan.local>
2015-08-11 17:49:28 -0600

    se corrigio espacio en tipostramite acciones

--- views/tipostramite/index.php
+++ views/tipostramite/index.php
@@ -36,7 +36,7 @@
 			<tr>
 				<td><?= $tt->nombre ?></td>
 				<td>
-					<?= Html::a('<span class="fa fa-pencil"></span',['tipostramite/update','id'=>$tt->id],['class'=>'btn btn-default btn-sm'])?>
+					<?= Html::a('<span class="fa fa-pencil"></span>',['tipostramite/update','id'=>$tt->id],['class'=>'btn btn-default btn-sm'])?>
 					<?= Html::a('Permisos',['pasostramite/index','id'=>$tt->id],['class'=>'btn btn-primary btn-sm'])?>
 				</td>
 			</tr>
