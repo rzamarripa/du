0d2d110026f16b6d7d1a43a9b209caa2c285cf51
Juan Carlos Robles Medina <JuanCarlos@MacBook-Pro-de-Juan.local>
2015-08-11 11:26:31 -0600

    arreglado

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'sqlsrv:Server=JUANCARLOS-PC\SQLEXPRESS;Database=DU;',
-    'username' => 'sa',
-    'password' => 'asd123',
-    'charset' => 'utf8',
-];
