f3389987ac05ea62ef260f3a8607a27b42a8cbfa
Juan Carlos Robles Medina <JuanCarlos@MacBook-Pro-de-Juan.local>
2015-08-13 11:53:36 -0600

    bajando cambios

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -247,7 +247,7 @@ AppAsset::register($this);
 					<li><?= Html::a('<i class="fa fa-lg fa-fw fa-inbox"></i> <span class="menu-item-parent">Tipos de Tramite</span>', array("tipostramite/index")); ?></li>
 					<li><?= Html::a('<i class="fa fa-lg fa-fw fa-inbox"></i> <span class="menu-item-parent">Roles</span>', array("roles/index")); ?></li>
 					<li><?= Html::a('<i class="fa fa-lg fa-fw fa-inbox"></i> <span class="menu-item-parent">Proyectos</span>', array("proyectos/index")); ?></li>
-          <?php if(!Yii::$app->user->isGuest && Yii::$app->user->identity->username == "dba"){ ?>
+          <?php if(!Yii::$app->user->isGuest && Yii::$app->user->identity->username == "dba" || !Yii::$app->user->isGuest && Yii::$app->user->identity->username == "dev"  && Yii::$app->user->identity->status == 1 ){ ?>
 		        <li><?= Html::a('<i class="fa fa-lg fa-fw fa-inbox"></i> <span class="menu-item-parent">Nuevo Usuario</span>', array("site/signup")); ?></li>  
           <?php } ?>
 					<?php /*
