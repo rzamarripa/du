6eb6f71ffb0305c184423f80852167eb9232be38
Roberto Zamarripa <roberto@masoft.mx>
2015-08-28 05:01:21 -0600

    vista update corregida

--- views/tramite-zonificacion/view.php
+++ views/tramite-zonificacion/view.php
@@ -7,7 +7,7 @@ use yii\widgets\DetailView;
 /* @var $model app\models\TramiteZonificacion */
 
 $this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Tramite Zonificación', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Trámite Zonificación', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="tramite-zonificacion-view">
@@ -15,14 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
+        <?= Html::a('Actualizar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
     </p>
     <?php echo $this->render("_solicitante",["model"=>$model]); ?>
 <?php /*
