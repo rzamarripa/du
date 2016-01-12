3b4131357b815a593a8245c752b5acca5ff679d4
Juan Carlos Robles Medina <JuanCarlos@MacBook-Pro-de-Juan.local>
2016-01-12 11:21:00 -0700

    planta construida

--- views/tramite-relotificacion/_form.php
+++ views/tramite-relotificacion/_form.php
@@ -605,21 +605,6 @@ $permisos= $model->permisosPorPaso;
 																											<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 						                                            <div class="row">
 						                                              <div class="col-sm-12">
-				                                                    <?= $form->field($model,'p1PlantaAltaXConstruir',[  'showLabels'=>true,
-					                                                    'showErrors'=>false,
-					                                                    //'addon' => ['prepend' => ['content'=>'<i class="fa fa-envelope fa-lg fa-fw"></i>']],
-					                                                    'options'=>['class' => 'form-group']]
-					                                                    )->input('text',[
-					                                                                        'class' => 'form-control input-lg',
-					                                                                        'placeholder'=>$model->getAttributeLabel('p1PlantaAltaXConstruir'),
-					                                                                        'name'=>'p1PlantaAltaXConstruir',
-					                                                                        'id'=>'p1PlantaAltaXConstruir'
-					                                                                    ]
-					                                                  );?> 
-						                                              </div>
-						                                            </div>
-						                                            <div class="row">
-						                                              <div class="col-sm-12">
 				                                                    <?= $form->field($model,'p1PlantaBajaXConstruir',[  'showLabels'=>true,
 					                                                    'showErrors'=>false,
 					                                                    //'addon' => ['prepend' => ['content'=>'<i class="fa fa-envelope fa-lg fa-fw"></i>']],
@@ -629,6 +614,21 @@ $permisos= $model->permisosPorPaso;
 					                                                                        'placeholder'=>$model->getAttributeLabel('p1PlantaBajaXConstruir'),
 					                                                                        'name'=>'p1PlantaBajaXConstruir',
 					                                                                        'id'=>'p1PlantaBajaXConstruir'
+					                                                                    ]
+					                                                  );?> 
+						                                              </div>
+						                                            </div>
+						                                            <div class="row">
+						                                              <div class="col-sm-12">
+				                                                    <?= $form->field($model,'p1PlantaAltaXConstruir',[  'showLabels'=>true,
+					                                                    'showErrors'=>false,
+					                                                    //'addon' => ['prepend' => ['content'=>'<i class="fa fa-envelope fa-lg fa-fw"></i>']],
+					                                                    'options'=>['class' => 'form-group']]
+					                                                    )->input('text',[
+					                                                                        'class' => 'form-control input-lg',
+					                                                                        'placeholder'=>$model->getAttributeLabel('p1PlantaAltaXConstruir'),
+					                                                                        'name'=>'p1PlantaAltaXConstruir',
+					                                                                        'id'=>'p1PlantaAltaXConstruir'
 					                                                                    ]
 					                                                  );?> 
 						                                              </div>
--- views/tramites-certificado-ocupacion/_form.php
+++ views/tramites-certificado-ocupacion/_form.php
@@ -388,24 +388,6 @@ $permisos= $model->permisosPorPaso;
 																								  <div class="panel-body">
 				                                            <div class="row">
 				                                                <div class="col-sm-12">
-				                                                    <?= $form->field($model,'p1PlantaAltaConstruida',[  'showLabels'=>true,
-
-				                                                                                        'showErrors'=>false,
-				                                                                                        //'addon' => ['prepend' => ['content'=>'<i class="fa fa-envelope fa-lg fa-fw"></i>']],
-				                                                                                        'options'=>['class' => 'form-group']]
-				                                                                                        )->input('text',[
-				                                                                                                            'class' => 'form-control input-lg',
-
-				                                                                                                            'placeholder'=>$model->getAttributeLabel('p1PlantaAltaConstruida'),
-				                                                                                                            'name'=>'p1PlantaAltaConstruida',
-				                                                                                                            'id'=>'p1PlantaAltaConstruida'
-
-				                                                                                                        ]
-				                                                                                        );?> 
-				                                                </div>
-				                                            </div>
-				                                            <div class="row">
-				                                                <div class="col-sm-12">
 
 				                                                    <?= $form->field($model,'p1PlantaBajaConstruida',[  'showLabels'=>true,
 
@@ -417,6 +399,24 @@ $permisos= $model->permisosPorPaso;
 				                                                                                                            'placeholder'=>$model->getAttributeLabel('p1PlantaBajaConstruida'),
 				                                                                                                            'name'=>'p1PlantaBajaConstruida',
 				                                                                                                            'id'=>'p1PlantaBajaConstruida'
+
+				                                                                                                        ]
+				                                                                                        );?> 
+				                                                </div>
+				                                            </div>
+				                                            <div class="row">
+				                                                <div class="col-sm-12">
+				                                                    <?= $form->field($model,'p1PlantaAltaConstruida',[  'showLabels'=>true,
+
+				                                                                                        'showErrors'=>false,
+				                                                                                        //'addon' => ['prepend' => ['content'=>'<i class="fa fa-envelope fa-lg fa-fw"></i>']],
+				                                                                                        'options'=>['class' => 'form-group']]
+				                                                                                        )->input('text',[
+				                                                                                                            'class' => 'form-control input-lg',
+
+				                                                                                                            'placeholder'=>$model->getAttributeLabel('p1PlantaAltaConstruida'),
+				                                                                                                            'name'=>'p1PlantaAltaConstruida',
+				                                                                                                            'id'=>'p1PlantaAltaConstruida'
 
 				                                                                                                        ]
 				                                                                                        );?> 
